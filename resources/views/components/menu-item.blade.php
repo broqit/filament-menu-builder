b92207307442e58c169386229306f482874f0309
Roman Borkunov <27624002+broqit@users.noreply.github.com>
2025-08-01 04:05:19 +0300

    Refactor menu builder views to streamline slot naming, enhance menu-item styles, and improve sortable functionality with fallback script.

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -11,32 +11,32 @@
 <li
     wire:key="{{ $item->getKey() }}"
     data-sortable-item="{{ $item->getKey() }}"
+    style="display: flex; flex-direction: column; gap: 20px"
     x-data="{ open: $persist(true).as('menu-item-' + {{ $item->getKey() }}) }"
 >
-    <div
-        class="flex justify-between px-3 py-2 bg-white shadow-sm rounded-xl ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10"
-    >
-        <div class="flex flex-1 items-center gap-2 truncate">
-            {{ $this->reorderAction }}
+    <div class="fi-section fi-header fi-align-justify fi-ta-cell">
+        <div class="fi-header fi-align-justif">
+            <div class="fi-ta-actions">
+                {{ $this->reorderAction }}
+                @if ($hasChildren)
+                    <x-filament::icon-button
+                        icon="heroicon-o-chevron-right"
+                        x-on:click="open = !open"
+                        x-bind:title="open ? '{{ trans('filament-menu-builder::menu-builder.items.collapse') }}' : '{{ trans('filament-menu-builder::menu-builder.items.expand') }}'"
+                        color="gray"
+                        class="transition duration-200 ease-in-out"
+                        x-bind:class="{ 'rotate-90': open }"
+                        size="sm"
+                        style="display: flex;"
+                    />
+                @endif
+                @if (\Datlechin\FilamentMenuBuilder\FilamentMenuBuilderPlugin::get()->isIndentActionsEnabled())
+                    {{ ($this->unindentAction)(['id' => $item->getKey()]) }}
+                    {{ ($this->indentAction)(['id' => $item->getKey()]) }}
+                @endif
+            </div>
 
-            @if ($hasChildren)
-                <x-filament::icon-button
-                    icon="heroicon-o-chevron-right"
-                    x-on:click="open = !open"
-                    x-bind:title="open ? '{{ trans('filament-menu-builder::menu-builder.items.collapse') }}' : '{{ trans('filament-menu-builder::menu-builder.items.expand') }}'"
-                    color="gray"
-                    class="transition duration-200 ease-in-out"
-                    x-bind:class="{ 'rotate-90': open }"
-                    size="sm"
-                />
-            @endif
-
-            @if (\Datlechin\FilamentMenuBuilder\FilamentMenuBuilderPlugin::get()->isIndentActionsEnabled())
-                {{ ($this->unindentAction)(['id' => $item->getKey()]) }}
-                {{ ($this->indentAction)(['id' => $item->getKey()]) }}
-            @endif
-
-            <div class="text-sm font-medium leading-6 text-gray-950 dark:text-white whitespace-nowrap">
+            <div class="fi-section-header-heading">
                 {{ $item->title }}
             </div>
 
@@ -44,10 +44,53 @@
                 {{ $item->url }}
             </div>
         </div>
-        <div class="flex items-center gap-2">
+        <div class="fi-ta-actions">
             <x-filament::badge :color="$item->type === 'internal' ? 'primary' : 'gray'" class="hidden sm:block">
                 {{ $item->type }}
             </x-filament::badge>
+
+            @once
+                <script type="text/javascript">
+                    // Забезпечуємо наявність функції menuBuilder глобально
+                    if (typeof window.menuBuilder === 'undefined') {
+                        console.log('Creating menuBuilder function as fallback');
+                        window.menuBuilder = function(config) {
+                            return {
+                                parentId: config.parentId,
+                                sortable: null,
+                                init: function() {
+                                    console.log('MenuBuilder init called for parentId:', this.parentId);
+                                    if (typeof Sortable !== 'undefined') {
+                                        var self = this;
+                                        var draggableSelector = '[data-sortable-item]';
+                                        var handleSelector = '[data-sortable-handle]';
+                                        var dataIdAttr = 'data-sortable-item';
+                                        this.sortable = new Sortable(this.$el, {
+                                            group: 'nested',
+                                            draggable: draggableSelector,
+                                            handle: handleSelector,
+                                            animation: 300,
+                                            ghostClass: 'fi-sortable-ghost',
+                                            dataIdAttr: dataIdAttr,
+                                            onSort: function() {
+                                                console.log('Sortable onSort triggered');
+                                                self.$wire.reorder(
+                                                    self.sortable.toArray(),
+                                                    self.parentId === 0 ? null : self.parentId
+                                                );
+                                            }
+                                        });
+                                    } else {
+                                        console.error('Sortable library not found');
+                                    }
+                                }
+                            };
+                        };
+                    } else {
+                        console.log('menuBuilder function already exists');
+                    }
+                </script>
+            @endonce
             {{ ($this->editAction)(['id' => $item->getKey(), 'title' => $item->title]) }}
             {{ ($this->deleteAction)(['id' => $item->getKey(), 'title' => $item->title]) }}
         </div>
@@ -59,6 +102,7 @@
         wire:key="{{ $item->getKey() }}.children"
         x-data="menuBuilder({ parentId: {{ $item->getKey()  }} })"
         class="mt-2 space-y-2 ms-4"
+        style="margin-left: 16px;"
     >
         @foreach ($item->children as $child)
             <x-filament-menu-builder::menu-item :item="$child" />
